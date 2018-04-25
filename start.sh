ddd95f419401b15dfe20ac7542a37fe699816c3b
David B <37987466+dalijolijo@users.noreply.github.com>
2018-04-25 17:57:37 +0200

    Only start supervisord

--- start.sh
+++ start.sh
@@ -2,26 +2,8 @@
 set -u
 
 #
-# Step 0/10 - User input
-#
-echo -n "Enter new password for [bitcore] user and Hit [ENTER]: "
-read BTXPWD
-echo bitcore:$BTXPWD | chpasswd
-
-#
 # Step 9/10 - Starting BitCore Service
 #
 # Hint: docker not supported systemd, use of supervisord
 printf "*** Step 9/10 - Starting BitCore Service ***\n"
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-printf "BitCore Server installed! Weeee!\n"
-printf "*** Done 9/10 ***\n\n"
-
-
-printf "\n*** Step 10/10 - Wait for wallet synchronization!***\n"
-printf "I advice to wait until wallet synchronization ends.\n"
-printf 'Easiest way to do it, is to catch new block message, where "height=***" is equal to "Current numbers of blocks" in local wallet (help>debug>information)\n\n'
-print 5
-printf "Now go, and visit our telegram channel at t.me/bitcore_btx_official tell us how its going!\n\n"
-read -p "Press enter to monitor debug.log"
-tail -f /home/bitcore/.bitcore/debug.log

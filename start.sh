43ddbf68912eab6f6df3dcb3a16de5d8b819f8aa
David B <37987466+dalijolijo@users.noreply.github.com>
2018-04-27 17:57:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ mkdir -p /home/bitcore/.bitcore
 chown -R bitcore:bitcore /home/bitcore
 cd /home/bitcore/.bitcore
 printf "** Step 8/10 - Downloading bootstrap file ***"
-if [ "$(curl -Is https://bitcore.cc/$BOOTSTRAP | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+if [ ! -f /home/bitcore/.bitcore/${BOOTSTRAP} ] && [ "$(curl -Is https://bitcore.cc/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         sudo -u bitcore wget https://bitcore.cc/$BOOTSTRAP; \
         sudo -u bitcore tar -xvzf $BOOTSTRAP; \
         rm $BOOTSTRAP; \

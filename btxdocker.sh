d90c5048ce8b55af8b0ba4407b89bbdb1a145193
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-04 10:41:35 +0200

    Create btxdocker.sh

---
+++ btxdocker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -u
+
+DOCKER_REPO='dalijolijo'
+
+#
+# Set bitcore user pwd
+#
+echo '*** Step 0/10 - User input ***'
+echo -n "Enter new password for [bitcore] user and Hit [ENTER]: "
+read PWD
+
+#
+# Check distro version (TODO)
+#
+#cat /etc/issue
+echo 'Checking OS version.'
+if [[ -r /etc/os-release ]]; then
+		. /etc/os-release
+		if [[ "${VERSION_ID}" != "16.04" ]]; then
+			echo "This script only supports ubuntu 16.04 LTS, exiting."
+			exit 1
+		fi
+fi
+
+#
+# Installation of docker package
+#
+apt-get update
+apt-get upgrade -y
+apt-get install docker.io -y
+docker pull ${DOCKER_REPO}/btx-rpc-server
+docker run -p 40332:40332 -p 8555:8555 -p 9051:9051 --name btx-rpc-server  -e BTXPWD='${PWD}' -v /home/bitcore:/home/bitcore:rw -d ${DOCKER_REPO}/btx-rpc-server

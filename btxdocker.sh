9a23a180bdc20ae3e780ced7e81bb32c158392b8
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-04 17:15:18 +0200

    disable distro check

--- btxdocker.sh
+++ btxdocker.sh
@@ -14,14 +14,14 @@ read PWD
 # Check distro version (TODO)
 #
 #cat /etc/issue
-echo 'Checking OS version.'
-if [[ -r /etc/os-release ]]; then
-		. /etc/os-release
-		if [[ "${VERSION_ID}" != "16.04" ]]; then
-			echo "This script only supports ubuntu 16.04 LTS, exiting."
-			exit 1
-		fi
-fi
+#echo 'Checking OS version.'
+#if [[ -r /etc/os-release ]]; then
+#		. /etc/os-release
+#		if [[ "${VERSION_ID}" != "16.04" ]]; then
+#			echo "This script only supports ubuntu 16.04 LTS, exiting."
+#			exit 1
+#		fi
+#fi
 
 #
 # Firewall settings (for Ubuntu)

3d451af1e4c6f491ad939854a527b7eb67969cfa
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-15 23:21:51 +0200

    Change from docker.io to docker-ce

--- btxdocker.sh
+++ btxdocker.sh
@@ -36,12 +36,28 @@ ufw allow 40332/tcp
 ufw default allow outgoing 
 yes | ufw enable
 
-
 #
-# Installation of docker package
+# Installation of docker-ce package (Ubuntu 16.04)
 #
 apt-get update
-apt-get upgrade -y
-apt-get install docker.io -y
+sudo apt-get remove -y docker \
+                       docker-engine \
+                       docker.io
+sudo apt-get install -y apt-transport-https \
+                        ca-certificates \
+                        curl \
+                        software-properties-common
+cd /root
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+
+#
+# Pull docker images and run the docker container
+#
 docker pull ${DOCKER_REPO}/btx-rpc-server
 docker run -p 40332:40332 -p 8555:8555 -p 9051:9051 --name btx-rpc-server  -e BTXPWD='${BTXPWD}' -v /home/bitcore:/home/bitcore:rw -d ${DOCKER_REPO}/btx-rpc-server

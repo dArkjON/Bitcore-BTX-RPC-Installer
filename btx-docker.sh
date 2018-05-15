f0009b4f33473816f5b81e0d602cdfaa13f713e2
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-15 23:44:38 +0200

    Check distro version

--- btx-docker.sh
+++ btx-docker.sh
@@ -4,24 +4,51 @@ set -u
 DOCKER_REPO='dalijolijo'
 
 #
+# Check distro version (TODO)
+#
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+if [[ $OS =~ "Ubuntu" ] && [ $VER =~ "16" ] || [[$OS =~ "ubuntu" ] && [ $VER =~ "16" ] ] ; then
+    echo "Installation for $OS ($VER)..."
+else
+    echo "$OS ($VER) not supported!"
+    exit
+fi
+
+#
 # Set bitcore user pwd
 #
 echo '*** Step 0/10 - User input ***'
 echo -n "Enter new password for [bitcore] user and Hit [ENTER]: "
 read BTXPWD
-
-#
-# Check distro version (TODO)
-#
-#cat /etc/issue
-#echo 'Checking OS version.'
-#if [[ -r /etc/os-release ]]; then
-#		. /etc/os-release
-#		if [[ "${VERSION_ID}" != "16.04" ]]; then
-#			echo "This script only supports ubuntu 16.04 LTS, exiting."
-#			exit 1
-#		fi
-#fi
 
 #
 # Firewall settings (for Ubuntu)

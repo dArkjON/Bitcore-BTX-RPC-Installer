42a5fc7f50269b36846a011b6cf15be69f48bcac
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-19 12:12:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,17 +12,17 @@ echo bitcore:${BTXPWD} | chpasswd
 # Step 6/10 - Configure bitcore.conf
 #
 printf "** Step 6/10 - Configure bitcore.conf ***\n"
-chown bitcore:bitcore /tmp/bitcore.conf
-chown bitcore:bitcore -R /home/bitcore/.bitcore
 sudo -u bitcore mkdir -p /home/bitcore/.bitcore	
+chown bitcore:bitcore -R /home/bitcore/
+chown bitcore:bitcore /tmp/bitcore.conf
 sudo -u bitcore cp /tmp/bitcore.conf /home/bitcore/.bitcore/bitcore.conf
 
 #
 # Step 8/10 - Downloading bootstrap file
 #
 printf "** Step 8/10 - Downloading bootstrap file ***\n"
+cd /home/bitcore/.bitcore/
 if [ ! -d /home/bitcore/.bitcore/blocks ] && [ "$(curl -Is https://bitcore.cc/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
-        cd /home/bitcore/.bitcore/; \
         sudo -u bitcore wget https://bitcore.cc/${BOOTSTRAP}; \
         sudo -u bitcore tar -xvzf ${BOOTSTRAP}; \
         sudo -u bitcore rm ${BOOTSTRAP}; \
